f044d2d91251e8960af509ed9b9b5ff76df2480d
nandaReddy1212 <nandareddy172@gmail.com>
2026-03-26 20:40:42 -0400

    switicing to local backed

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   # Store state in GCS so it's not lost locally
-  backend "gcs" {
-    bucket = "vertexai-489303-tf-state"
-    prefix = "riskplatform/state"
-  }
+  # backend "gcs" {
+  #   bucket = "vertexai-489303-tf-state"
+  #   prefix = "riskplatform/state"
+  # }
 }
 
 provider "google" {
